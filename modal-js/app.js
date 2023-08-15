6d54345e329fefe5a578419b35e947de246784fd
Salman <muhammadsalman410@gmail.com>
2023-08-15 22:07:58 +0500

    modal js added full

---
+++ modal-js/app.js
@@ -0,0 +1,19 @@
+const openModal = document.querySelector("#open-modal");
+const closeModal = document.querySelector("#close-modal");
+
+const modal = document.querySelector(".modal-div");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+console.log(openModal);
+console.log(modal);
+console.log(modalOverlay);
+
+openModal.addEventListener("click", function () {
+  modal.style.visibility = "visible";
+  modalOverlay.style.visibility = "visible";
+});
+
+closeModal.addEventListener("click", function () {
+  modal.style.visibility = "hidden";
+  modalOverlay.style.visibility = "hidden";
+});
